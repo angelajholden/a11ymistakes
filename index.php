57703136378054a01f10e6b49f50dcf4bdabacbf
Angela J. Holden <hold4477@stthomas.edu>
2020-12-06 21:40:37 -0600

    updating path

--- index.php
+++ index.php
@@ -3,5 +3,5 @@
 To deploy static HTML projects, we need to let Heroku think this is a PHP app 
 If you don't need this, delete index.php and change the name of index.html to index.html 
 */
-include_once('dist/index.html'); 
+include_once('index.html'); 
 ?>
